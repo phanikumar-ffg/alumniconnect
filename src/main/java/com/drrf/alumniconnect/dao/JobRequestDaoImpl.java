e1ec2e00b95dbb04f3d1c870c37e81d7ef792ed7
Jayesh Goyal <arj.goyal207@gmail.com>
2020-05-11 23:57:48 +0530

    Added insert query for job application status table when new job request comes

--- src/main/java/com/drrf/alumniconnect/dao/JobRequestDaoImpl.java
+++ src/main/java/com/drrf/alumniconnect/dao/JobRequestDaoImpl.java
@@ -1,9 +1,11 @@
 package com.drrf.alumniconnect.dao;
 
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.drrf.alumniconnect.model.JobRequest;
 import com.drrf.alumniconnect.model.Mail;
@@ -11,8 +13,10 @@ import com.drrf.alumniconnect.service.MailService;
 import com.drrf.alumniconnect.utils.APIUtils;
  
 @Repository
-public class JobRequestDaoImpl implements JobRequestDao {
+public class JobRequestDaoImpl implements JobRequestDao{
 	private static final Logger logger = LoggerFactory.getLogger(JobRequestDaoImpl.class);
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
 
 	@Autowired
 	MailService mailService;
@@ -26,7 +30,7 @@ public class JobRequestDaoImpl implements JobRequestDao {
 			if (jobReq == null ) {
 				throw new Exception("No Request Data Found");
 			}else {
-				
+				this.makeDBInsert(jobReq);
 				String emailBody= "New Job Request\n\nPlease find the details below.\n\n\n"
                         +"User ID: "+jobReq.getStudentId()+" \nStudent_Name: "+jobReq.getStudentName()+
                         "\nJob_ID: "+ jobReq.getJobId()
@@ -45,6 +49,21 @@ public class JobRequestDaoImpl implements JobRequestDao {
             message = "Sending Job Request Failed!!";
         }
         return message;
+	}
+
+	public void makeDBInsert(JobRequest jobReq) throws Exception{
+		try {
+			String sql = "INSERT INTO tbl_job_application_status (STUDENT_ID, JOB_ID, APPLICATION_STATUS, CREATE_TIMESTAMP) VALUES (?,?,?,?)";
+			int i = jdbcTemplate.update(sql, new Object[] { jobReq.getStudentId(), jobReq.getJobId(), "Submitted", new Date() });
+			if(i==0){
+				throw new Exception( "Error occured while saving job request details");
+			}
+		}
+		catch(Exception e){
+			logger.error(e.getLocalizedMessage(),e);
+			throw new Exception( "Error occured while saving job request details");
+		}
+
 	}
 
 }
