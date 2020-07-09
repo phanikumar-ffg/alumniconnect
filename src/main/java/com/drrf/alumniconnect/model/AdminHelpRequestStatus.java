7de195a415d66c9b73f454d7697aec8fdcc98b0a
praneeth-kumar-ffg <praneethckumar@gmail.com>
2020-07-09 21:00:59 +0530

    fixed all duplicate requests errors admin help request service

--- src/main/java/com/drrf/alumniconnect/model/AdminHelpRequestStatus.java
+++ src/main/java/com/drrf/alumniconnect/model/AdminHelpRequestStatus.java
@@ -3,16 +3,52 @@ package com.drrf.alumniconnect.model;
 import java.sql.Timestamp;
 
 public class AdminHelpRequestStatus {
-    private long StudentId;
+    private long AspirantId;
+    private String reason;
+    private String details;
+    private String centerId;
+    private String description;
     private String status;
     private Timestamp createdTime;
 
-    public long getStudentId() {
-        return StudentId;
+    public long getAspirantId() {
+        return AspirantId;
     }
 
-    public void setStudentId(long studentId) {
-        StudentId = studentId;
+    public void setAspirantId(long aspirantId) {
+        AspirantId = aspirantId;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getCenterId() {
+        return centerId;
+    }
+
+    public void setCenterId(String centerId) {
+        this.centerId = centerId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getStatus() {
