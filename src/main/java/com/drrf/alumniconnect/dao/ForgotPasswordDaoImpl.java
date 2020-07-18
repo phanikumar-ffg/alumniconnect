cf6d9c0ce1becb2b00ea04b1863407050f65157e
phanikumar-ffg <59959469+phanikumar-ffg@users.noreply.github.com>
2020-07-18 17:10:13 +0530

    Update ForgotPasswordDaoImpl.java

--- src/main/java/com/drrf/alumniconnect/dao/ForgotPasswordDaoImpl.java
+++ src/main/java/com/drrf/alumniconnect/dao/ForgotPasswordDaoImpl.java
@@ -1,50 +1,68 @@
-package com.drrf.alumniconnect.controller;
-import javax.mail.AuthenticationFailedException;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.MediaType;
+package com.drrf.alumniconnect.dao;
 
+import javax.mail.AuthenticationFailedException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.drrf.alumniconnect.exceptions.ForgotPasswordDaoException;
-import com.drrf.alumniconnect.service.ForgotPasswordService;
+import com.drrf.alumniconnect.jdbcmapper.LoginDetailsRowMapper;
+import com.drrf.alumniconnect.model.LoginDetails;
+import com.drrf.alumniconnect.model.Mail;
+import com.drrf.alumniconnect.service.MailService;
 import com.drrf.alumniconnect.utils.APIUtils;
-import com.google.gson.JsonObject;
+ 
+@Repository
+public class ForgotPasswordDaoImpl implements ForgotPasswordDao {
+	private static final Logger logger = LoggerFactory.getLogger(ForgotPasswordDaoImpl.class);
 
-
-@Path("/api/v1")
-public class ForgotPasswordController {
-	private static final Logger logger = LoggerFactory.getLogger(ForgotPasswordController.class);
 	@Autowired
-	private ForgotPasswordService forgotPasswordService;
-	
-	@GET
-	@Path("/forgotpassword/{email}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getForgotPassword(@PathParam(value = "email") String email) {
-		logger.info("Received request for get password information");
-		
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	MailService mailService;
+
+	@SuppressWarnings("unused")
+	@Override
+	public String getForgotPasswordInformation(String email) throws ForgotPasswordDaoException, AuthenticationFailedException{
+
+		//List<LoginDetails> loginDetailsList = new ArrayList<LoginDetails>();
+		LoginDetails loginDetails = null;
+		String message = "";
+
 		try {
-			//responseObj.put("", forgotPasswordService.getForgotPasswordInformation(email));
-			return Response.ok().entity(forgotPasswordService.getForgotPasswordInformation(email)).build();
-		} catch (ForgotPasswordDaoException e) {	
-			JsonObject error=new JsonObject();
-			error.addProperty(APIUtils.ERROR_MESSAGE, e.getLocalizedMessage());
-			return Response.status(Status.BAD_REQUEST).entity(error.toString()).build();
-		}catch (AuthenticationFailedException e) {	
-			JsonObject error=new JsonObject();
-			error.addProperty(APIUtils.ERROR_MESSAGE, e.getLocalizedMessage());
-			return Response.status(Status.BAD_REQUEST).entity(error.toString()).build();
+			String sql = "SELECT sr_no, email_id,password FROM tbl_login_details where email_id = ?";
+
+			loginDetails = jdbcTemplate.queryForObject(sql, new Object[]{email}, new LoginDetailsRowMapper());
+
+
+			if (loginDetails == null ) {
+				throw new ForgotPasswordDaoException( String.format("User [%s]  information not available in the database", email));
+			}else {
+				//write the code to send details to email and phone
+				//need smtp details
+				String emailBody= "Dr Reddy's Foundation welcome you,\n\nYour request for password deteails is successful. Please find the details below.\n\n\n"
+						+ "User ID: "+loginDetails.getEmailId()+" \nPassword: "+loginDetails.getPassword()+"\n \n Regards,\n Dr Reddy Foundation";
+
+		        Mail mail = new Mail();
+		        mail.setMailFrom(APIUtils.MAIL_FROM);
+		        mail.setMailTo(APIUtils.MAIL_TO);
+		        mail.setMailSubject(APIUtils.MAIL_FORGOT_PWD_SUB);
+		        mail.setMailContent(emailBody);
+		        mailService.sendEmail(mail);
+
+				message ="Your login ID and password is sent to your email ID:"+ loginDetails.getEmailId() +" and registered mobile number. If you don't get the details in 5 min, please contact the admin. Admin E-mail ID: abc@gmail.com";
+			}
+		} catch (ForgotPasswordDaoException e) {
+			throw e;
+		}catch(Exception e) {
+			logger.error(e.getLocalizedMessage(),e);
+			throw new ForgotPasswordDaoException( "Error occured while checking the user account information " +  email);
 		}
 
-
+		return message;
 	}
-	
+
 }
