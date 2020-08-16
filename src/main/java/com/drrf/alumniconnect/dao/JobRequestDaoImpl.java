441bb218707b3d27bddce760f1cbf82f01c64dbf
Jayesh Goyal <jayesh.goyal5@gmail.com>
2020-08-17 00:11:32 +0530

    Added send grid mailing in job request

--- src/main/java/com/drrf/alumniconnect/dao/JobRequestDaoImpl.java
+++ src/main/java/com/drrf/alumniconnect/dao/JobRequestDaoImpl.java
@@ -8,8 +8,14 @@ import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.drrf.alumniconnect.model.JobRequest;
-import com.drrf.alumniconnect.model.Mail;
-import com.drrf.alumniconnect.service.MailService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.*;
 import com.drrf.alumniconnect.utils.APIUtils;
  
 @Repository
@@ -17,53 +23,6 @@ public class JobRequestDaoImpl implements JobRequestDao{
 	private static final Logger logger = LoggerFactory.getLogger(JobRequestDaoImpl.class);
 	@Autowired
     private JdbcTemplate jdbcTemplate;
-
-	@Autowired
-	MailService mailService;
-
-	@Override
-	public String sendJobRequest(JobRequest jobReq) {
-
-        String message = "";
-		try {
-			
-			if (jobReq == null ) {
-				throw new Exception("No Request Data Found");
-			}else {
-				String emailBody= "An interest has been expressed for the following job opening by "+ 
-						jobReq.getStudentName().toUpperCase()+" (Aspirant id - "+jobReq.getStudentId()+")\n\n"
-						+"Job ID: "+ jobReq.getJobId() + "\nCompany Name: "+ jobReq.getJobCompanyName()+
-						"\nRole/Designation: "+ jobReq.getJobRole() +"\nJob Description: "+ jobReq.getJobDescription()
-						+"\n\nRegards,\nTeam DRF Grow"
-                        ;
-				
-		        Mail mail = new Mail();
-		        mail.setMailFrom(APIUtils.MAIL_FROM);
-		        mail.setMailTo(APIUtils.MAIL_TO);
-		        mail.setMailSubject(APIUtils.MAIL_JOB_REQ_SUB);
-				mail.setMailContent(emailBody);
-				mail.setMailCc(jobReq.getStudentEmail());
-				try {
-					logger.info("Sending mail to: "+jobReq.getStudentEmail());
-					mailService.sendEmail(mail);
-					logger.info("Mail successfully sent");
-					this.makeDBInsert(jobReq);
-					logger.info("DB insert for job request was Successful");
-					message = "Job Request Successfully sent!!";
-					return message;
-				}
-				catch(Exception e){
-					logger.error("Error: "+e.getLocalizedMessage());
-					message = "Sending Job Request Failed";
-					return message;
-				}
-
-		    }
-		}catch(Exception e) {
-			logger.error("Error: "+e.getLocalizedMessage());
-			return message;
-        }
-	}
 
 	public void makeDBInsert(JobRequest jobReq) throws Exception{
 		try {
@@ -82,6 +41,77 @@ public class JobRequestDaoImpl implements JobRequestDao{
 			throw new Exception( "Error occured while saving job request details");
 		}
 
+	}
+
+	private static void sendMail(final Mail mail) throws Exception{
+		final SendGrid sg = new SendGrid(APIUtils.SENDGRID_API_KEY);
+		Request request = new Request();
+		request.setMethod(Method.POST);
+		request.setEndpoint("mail/send");
+		request.setBody(mail.build());
+
+		Response response = sg.api(request);
+		logger.info(String.valueOf(response.getStatusCode()));
+		logger.info(response.getBody());
+		logger.info(String.valueOf(response.getHeaders()));
+ 	 }
+
+	 public static Mail mailBuilder(JobRequest jobReq) {
+		 String emailBody= "An interest has been expressed for the following job opening by "+ 
+						jobReq.getStudentName().toUpperCase()+" (Aspirant id - "+jobReq.getStudentId()+")\n\n"
+						+"Job ID: "+ jobReq.getJobId() + "\nCompany Name: "+ jobReq.getJobCompanyName()+
+						"\nRole/Designation: "+ jobReq.getJobRole() +"\nJob Description: "+ jobReq.getJobDescription()
+						+"\n\nRegards,\nTeam DRF Grow"
+                        ;
+
+		Mail mail = new Mail();
+
+		Email fromEmail = new Email(APIUtils.MAIL_FROM);
+		Content content = new Content("text/plain", emailBody);
+		Personalization personalization = new Personalization();
+		Email to = new Email(APIUtils.MAIL_TO);
+		personalization.addTo(to);
+		Email cc = new Email(jobReq.getStudentEmail());
+		personalization.addCc(cc);
+
+		mail.setFrom(fromEmail);
+		mail.setSubject(APIUtils.MAIL_JOB_REQ_SUB);
+		mail.addContent(content);
+		mail.addPersonalization(personalization);
+		return mail;
+  	}
+
+  @Override
+	public String sendJobRequest(JobRequest jobReq) {
+
+        String message = "";
+		try {
+			
+			if (jobReq == null ) {
+				throw new Exception("No Request Data Found");
+			}else {
+
+				try {
+					logger.info("Sending mail to: "+jobReq.getStudentEmail());
+					final Mail jobReqMail = mailBuilder(jobReq);
+					sendMail(jobReqMail);
+					logger.info("Mail successfully sent");
+					this.makeDBInsert(jobReq);
+					logger.info("DB insert for job request was Successful");
+					message = "Job Request Successfully sent!!";
+					return message;
+				}
+				catch(Exception e){
+					logger.error("Error: "+e.getLocalizedMessage());
+					message = "Sending Job Request Failed";
+					return message;
+				}
+
+		    }
+		}catch(Exception e) {
+			logger.error("Error: "+e.getLocalizedMessage());
+			return message;
+        }
 	}
 
 }
