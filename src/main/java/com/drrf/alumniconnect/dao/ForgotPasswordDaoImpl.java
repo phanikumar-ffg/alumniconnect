33e18019201a8a4559a23a38309d73b261f9ef1d
Phani Kumar <phanikumar0610@gmail.com>
2020-08-16 12:03:41 +0530

    Integrated with SendGrid API for sending emails in Azure.

--- src/main/java/com/drrf/alumniconnect/dao/ForgotPasswordDaoImpl.java
+++ src/main/java/com/drrf/alumniconnect/dao/ForgotPasswordDaoImpl.java
@@ -5,6 +5,7 @@ import javax.mail.AuthenticationFailedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,18 +13,21 @@ import org.springframework.stereotype.Repository;
 import com.drrf.alumniconnect.exceptions.ForgotPasswordDaoException;
 import com.drrf.alumniconnect.jdbcmapper.LoginDetailsRowMapper;
 import com.drrf.alumniconnect.model.LoginDetails;
-import com.drrf.alumniconnect.model.Mail;
-import com.drrf.alumniconnect.service.MailService;
 import com.drrf.alumniconnect.utils.APIUtils;
- 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
 @Repository
 public class ForgotPasswordDaoImpl implements ForgotPasswordDao {
 	private static final Logger logger = LoggerFactory.getLogger(ForgotPasswordDaoImpl.class);
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	@Autowired
-	MailService mailService;
 
 	@SuppressWarnings("unused")
 	@Override
@@ -37,25 +41,35 @@ public class ForgotPasswordDaoImpl implements ForgotPasswordDao {
 			String sql = "SELECT sr_no, email_id,password FROM tbl_login_details where email_id = ?";
 
 			loginDetails = jdbcTemplate.queryForObject(sql, new Object[]{email}, new LoginDetailsRowMapper());
-				//write the code to send details to email and phone
-				//need smtp details
-				logger.info("User found and getting ready to send email.");
-				String emailBody= "Dr Reddy's Foundation welcome you,\n\nYour request for password deteails is successful. Please find the details below.\n\n\n"
-						+ "User ID: "+loginDetails.getEmailId()+" \nPassword: "+loginDetails.getPassword()+"\n \n Regards,\n Dr Reddy Foundation";
+			//write the code to send details to email and phone
+			//need smtp details
+			logger.info("User found and getting ready to send email.");
+			String emailBody= "Dr Reddy's Foundation welcome you,\n\nYour request for password deteails is successful. Please find the details below.\n\n\n"
+					+ "User ID: "+loginDetails.getEmailId()+" \nPassword: "+loginDetails.getPassword()+"\n \n Regards,\n Dr Reddy Foundation";
 
-		        Mail mail = new Mail();
-		        mail.setMailFrom(APIUtils.MAIL_FROM);
-		        mail.setMailTo(loginDetails.getEmailId());
-		        mail.setMailSubject(APIUtils.MAIL_FORGOT_PWD_SUB);
-		        mail.setMailContent(emailBody);
-		        mailService.sendEmail(mail);
-            logger.info("mail sent");
-				    message ="Your login ID and password is sent to your email ID "+ loginDetails.getEmailId() +" and registered mobile number. If you dont get the details in 5 min, please contact the admin. Admin E-mail ID abc@gmail.com";
+
+			Email from = new Email(APIUtils.MAIL_FROM);
+			String subject = APIUtils.MAIL_FORGOT_PWD_SUB;
+			Email to = new Email(loginDetails.getEmailId());
+			Content content = new Content("text/plain", emailBody);
+			Mail mail = new Mail(from, subject, to, content);
+
+			SendGrid sg = new SendGrid(APIUtils.SENDGRID_API_KEY);
+			Request request = new Request();
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			logger.info(String.valueOf(response.getStatusCode()));
+			logger.info(response.getBody());
+			logger.info(String.valueOf(response.getHeaders()));
+			logger.info("mail sent");
+			message ="Your login ID and password is sent to your email ID "+ loginDetails.getEmailId() +" and registered mobile number. If you dont get the details in 5 min, please contact the admin. Admin E-mail ID abc@gmail.com";
 		}catch (EmptyResultDataAccessException e){
-        throw new ForgotPasswordDaoException( String.format("User information not available in the database"));
-    } catch(Exception e) {
-			  logger.error(e.getLocalizedMessage(),e);
-			  throw new ForgotPasswordDaoException( "Error occured while checking the user account information " +  email);
+			throw new ForgotPasswordDaoException( String.format("User information not available in the database"));
+		} catch(Exception e) {
+			logger.error(e.getLocalizedMessage(),e);
+			throw new ForgotPasswordDaoException( "Error occured while checking the user account information " +  email);
 		}
 
 		return message;
--- src/main/java/com/drrf/alumniconnect/utils/APIUtils.java
+++ src/main/java/com/drrf/alumniconnect/utils/APIUtils.java
@@ -10,5 +10,6 @@ public class APIUtils  {
 	public static final String MAIL_JOB_REQ_SUB = "Job Apllication";
 	public static final String MAIL_NEW_USR_SUB = "New User request";
 	public static final String MAIL_HELP_REQUEST="Received a Help Request from Student";
+	public static final String SENDGRID_API_KEY="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";
 
 }
